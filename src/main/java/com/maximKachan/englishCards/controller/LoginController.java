8e902a9dccf03d3290a91361477619390a18d55f
maxiKachan <max101111427@gmail.com>
2022-01-27 17:30:52 +0300

    Added welcome.jsp and change LoginController.java

--- src/main/java/com/maximKachan/englishCards/controller/LoginController.java
+++ src/main/java/com/maximKachan/englishCards/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.maximKachan.englishCards.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -25,11 +26,15 @@ public class LoginController {
     }
 
     @PostMapping()
-    public String authorizeUser(@RequestParam("username") String username, @RequestParam("password") String password){
+    public String authorizeUser(@RequestParam("username") String username,
+                                @RequestParam("password") String password,
+                                Model model){
         User user = userService.findUserByEmail(username);
         if (user.getPassword().equals(password)) {
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("id", user.getId());
             log.info("authorizeUser");
-            return "redirect:/cards/" + user.getId();
+            return "welcome";
         }
         System.out.println(user);
         return "login";
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8"%>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h2>Hello, ${username}</h2>
+    <a href="/cards/${id}">cards</a>
+</body>
+</html>
