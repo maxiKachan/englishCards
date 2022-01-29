ac6c9c81fdd7a07258dbc03024df6390b837a8c1
maxiKachan <max101111427@gmail.com>
2022-01-29 21:49:05 +0300

    Added method in enums EnglishLevel.java and Type.java which return enum by string

--- src/main/java/com/maximKachan/englishCards/model/EnglishLevel.java
+++ src/main/java/com/maximKachan/englishCards/model/EnglishLevel.java
@@ -19,4 +19,17 @@ public enum EnglishLevel {
     public String toString() {
         return description;
     }
+
+    public static EnglishLevel getEnglishLever(String level){
+        switch (level){
+            case "A1": return EnglishLevel.A1;
+            case "A2": return EnglishLevel.A2;
+            case "B1": return EnglishLevel.B1;
+            case "B2": return EnglishLevel.B2;
+            case "C1": return EnglishLevel.C1;
+            case "C2": return EnglishLevel.C2;
+            case "UNDEFINED": return EnglishLevel.UNDEFINED;
+            default: throw new RuntimeException("Wrong english level");
+        }
+    }
 }
--- src/main/java/com/maximKachan/englishCards/model/Type.java
+++ src/main/java/com/maximKachan/englishCards/model/Type.java
@@ -12,5 +12,23 @@ public enum Type {
     PHRASE,
     PREPOSITION,
     PRONOUN,
-    VERB
+    VERB;
+
+    public static Type getType(String type){
+        switch (type){
+            case "ADJECTIVE": return Type.ADJECTIVE;
+            case "ADVERB": return Type.ADVERB;
+            case "AUXILIARY_VERB": return Type.AUXILIARY_VERB;
+            case "DETERMINER": return Type.DETERMINER;
+            case "EXCLAMATION": return Type.EXCLAMATION;
+            case "MODAL_VERB": return Type.MODAL_VERB;
+            case "NOUN": return Type.NOUN;
+            case "PHRASAL_VERB": return Type.PHRASAL_VERB;
+            case "PHRASE": return Type.PHRASE;
+            case "PREPOSITION": return Type.PREPOSITION;
+            case "PRONOUN": return Type.PRONOUN;
+            case "VERB": return Type.VERB;
+            default: throw new RuntimeException("Wrong parameter type");
+        }
+    }
 }
